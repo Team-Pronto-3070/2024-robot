524b0b37b0220d44650b85560ef2975804b4cbed
TurtleWhal <garrettrjordan@gmail.com>
2024-01-22 16:10:22 -0800

    Added missing swerve configuration to robotcontainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,16 +4,39 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.SwerveSubsystem;
 
 public class RobotContainer {
 
+  private final OI oi = new OI(Constants.OI.driverPort);
+  private final SwerveSubsystem swerve = new SwerveSubsystem();
+
   public RobotContainer() {
+    DataLogManager.start();
+    DriverStation.startDataLog(DataLogManager.getLog());
+
+    swerve.setDefaultCommand(swerve.run(() -> swerve.drive(
+        Math.pow(MathUtil.applyDeadband(oi.drive_x.getAsDouble(), Constants.OI.deadband), 3) * Constants.Swerve.maxSpeed
+            * (oi.driveSlow.getAsBoolean() ? Constants.OI.slowSpeed : 1),
+        Math.pow(MathUtil.applyDeadband(oi.drive_y.getAsDouble(), Constants.OI.deadband), 3) * Constants.Swerve.maxSpeed
+            * (oi.driveSlow.getAsBoolean() ? Constants.OI.slowSpeed : 1),
+        Math.pow(MathUtil.applyDeadband(oi.drive_rot.getAsDouble(), Constants.OI.deadband), 3)
+            * Constants.Swerve.maxAngularSpeed * (oi.driveSlow.getAsBoolean() ? 0.15 : 1),
+        true,
+        true)));
+
+    oi.gyroResetButton.onTrue(swerve.runOnce(swerve::resetGyro));
+
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
