a1d91607e9d17a5753afaccbec4bfafd6910c0f4
TurtleWhal <garrettrjordan@gmail.com>
2024-02-05 17:18:01 -0800

    Changed motor to CANSparkMax

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,6 +2,8 @@
 
 package frc.robot;
 
+import com.revrobotics.SparkLimitSwitch;
+import com.revrobotics.CANSparkBase.IdleMode;
 
 public class Constants {
     public static final class IntakeSubsystem {
@@ -9,6 +11,9 @@ public class Constants {
         public static final int limitSwitchPort = 0;
         public static final double runningTime = 0;
         public static final int intakeMotorID = 0;
+        public static final IdleMode idleMode = IdleMode.kBrake;
+        public static final SparkLimitSwitch.Type limitSwitchPolarity = SparkLimitSwitch.Type.kNormallyClosed;
+
         public static final int driverPort = 0;
     }
 
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,8 +1,8 @@
 package frc.robot.subsystems;
 //need to import the motor
 
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -10,12 +10,12 @@ import frc.robot.Constants;
 
 public class IntakeSubsystem extends SubsystemBase {
     //need to initialize the motor
-    private final WPI_TalonSRX intakeMotor; 
+    private final CANSparkMax intakeMotor;
 
     public IntakeSubsystem() {
-        intakeMotor = new WPI_TalonSRX(Constants.IntakeSubsystem.intakeMotorID);
-        intakeMotor.configFactoryDefault();
-        intakeMotor.setNeutralMode(NeutralMode.Brake);
+        intakeMotor = new CANSparkMax(Constants.IntakeSubsystem.intakeMotorID, MotorType.kBrushless);
+        intakeMotor.restoreFactoryDefaults();
+        intakeMotor.setIdleMode(Constants.IntakeSubsystem.idleMode);
     }
     
     public Command motorStop() {
@@ -27,13 +27,13 @@ public class IntakeSubsystem extends SubsystemBase {
     //}
 
     public Command run() {
-        if (intakeMotor.isFwdLimitSwitchClosed() == 1) {
+        if (intakeMotor.getForwardLimitSwitch(Constants.IntakeSubsystem.limitSwitchPolarity).isPressed()) {
             //motorRun();
             return this.motorStop();
         } else {
             //motorStop();
             return this.run( ()-> intakeMotor.set(Constants.IntakeSubsystem.runningSpeed))
-                .until(()-> intakeMotor.isFwdLimitSwitchClosed() == 1);
+                .until(()-> intakeMotor.getForwardLimitSwitch(Constants.IntakeSubsystem.limitSwitchPolarity).isPressed());
         }
         
     } 
